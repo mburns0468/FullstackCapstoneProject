59d1b9be5f00c81b23ea526598a3323350fc3014
mburns0468 <134165051+mburns0468@users.noreply.github.com>
2023-12-05 15:01:29 -0500

    drinksearch.js

--- drinksearch.js
+++ drinksearch.js
@@ -1,64 +1,30 @@
-document.getElementById('drink-search').addEventListener('submit', function (event) {
-    event.preventDefault();
-    searchCocktail();
-  });
-  
-  async function searchCocktail() {
-    const searchTerm = document.getElementById('dSearch').value;
-    
-    const url = `https://the-cocktail-db.p.rapidapi.com/search.php?s=${searchTerm}`;
-    const options = {
-      method: 'GET',
+document.getElementById('drink-search').addEventListener('submit', function(event) {
+  event.preventDefault();
+
+  // Get form data
+  const searchInput = document.getElementById('dSearch').value;
+
+  // setting API gateway endpoint to variable
+  const apiGatewayEndpoint = 'https://15aw1pweoa.execute-api.us-east-1.amazonaws.com/drinksearchpost';
+
+  // Make a POST request to Lambda through API gateway endpoint
+  fetch(apiGatewayEndpoint, {
+      method: 'POST',
+      body: JSON.stringify({ searchInput }),
       headers: {
-        'X-RapidAPI-Key': '29c38d7341msh7a15b3f6f2fbf49p192425jsn6de4d45dc56e',
-        'X-RapidAPI-Host': 'the-cocktail-db.p.rapidapi.com'
+          'Content-Type': 'application/json',
+          // Add any AWS authentication headers here if required
       }
-    };
+  })
+  .then(response => response.text())
+  .then(htmlContent => {
+  // Handle the API response here and update your web page with the data
+      displayHTML(htmlContent);
+  })    
+  .catch(error => console.error(error));
   
-    try {
-      const response = await fetch(url, options);
-      const data = await response.json();
-  
-      displayResults(data);
-    } catch (error) {
-      console.error(error);
-    }
-  }
-  
-  function displayResults(data) {
-    const resultsContainer = document.getElementById('cocktail-results');
-    resultsContainer.innerHTML = '';
-  
-    if (data.drinks) {
-      data.drinks.forEach(drink => {
-        const drinkDetails = `
-          <h2>${drink.strDrink}</h2>
-          <p><strong>Instructions:</strong> ${drink.strInstructions}</p>
-          <p><strong>Glass:</strong> ${drink.strGlass}</p>
-          <p><strong>Ingredients:</strong></p>
-          <ul>
-            ${getIngredientsList(drink)}
-          </ul>
-          <hr>
-        `;
-        resultsContainer.innerHTML += drinkDetails;
-      });
-    } else {
-      resultsContainer.innerHTML = '<p>No results found.</p>';
-    }
-  }
-  
-  function getIngredientsList(drink) {
-    let ingredientsList = '';
-    for (let i = 1; i <= 15; i++) {
-      const ingredient = drink[`strIngredient${i}`];
-      const measure = drink[`strMeasure${i}`];
-  
-      if (ingredient && measure) {
-        ingredientsList += `<li>${measure} ${ingredient}</li>`;
-      } else if (ingredient) {
-        ingredientsList += `<li>${ingredient}</li>`;
+  function displayHTML(htmlContent) {
+          const apiResponseContainer = document.getElementById('resultContainer');
+          apiResponseContainer.innerHTML = htmlContent;
       }
-    }
-    return ingredientsList;
-  }
+});
